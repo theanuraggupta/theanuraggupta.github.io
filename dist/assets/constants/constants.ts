c0526f8aec9165af4be4ebb3c23815a94f0e01cd
Anurag Gupta <email@anuraggupta.in>
2019-12-28 10:31:14 +0530

    Updates ~ Design Changes and Folder structure changes

--- dist/assets/constants/constants.ts
+++
@@ -1,7 +0,0 @@
-export const APPLICATION_CONSTANTS  = {
-    'about': `<h4><em>Hello there,</em> my name is</h4>
-    <h2>Anurag Gupta</h2>
-    <p>I’m a 26 year old enthusiastic UI developer with <span class="abouthighlight">three and half years of IT experience.</span>&nbsp;Specialising in interactive and progressive web application development with frameworks of javascript.</p>
-    <p>Over the years, I have found a new curiosity for anything digital and making technology easy to use and accessible to everyone (feels good to do good).</p>
-    <p>Feel free to <a class="highlight" href="mailto:email@anuraggupta.in?Subject=Hello%20Anurag">contact</a> me here.</p>`
-};
